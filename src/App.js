96af99eb0a8144976a86859699b961450851f1f3
praveen sundar s <praveensundars6@gmail.com>
2024-01-04 21:12:59 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import axios from 'axios';
 
 function App() {
+
+  const[name,setName]=useState("")
+  const [data,setData]=useState({
+    name:"",
+    celcius:"",
+    speed:"",
+    hum:"",
+    climate:""
+
+  });
+  
+  const handleClick = ()=>{
+    if(name!==""){
+      const url=`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=bb4578bc45ceab9d87939dd06c7724d6&units=metric`;
+      axios.get(url)
+      .then(res =>{
+        console.log(res.data);
+        setData({...data, celcius:res.data.main.temp, name:res.data.name, speed:res.data.wind.speed, climate:res.data.weather.description, hum:res.data.main.humidity})
+      }).catch(err=>console.log(alert("Invalid Name")))
+      setName("")
+    }
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section id="home">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6">
+
+          <div className="card">
+          <div className="col-md-8">
+           <div className="d-flex text-center">
+               <input className="form-control" placeholder="Search place...." type="text" value={name} onChange={e=>setName(e.target.value)}/>
+               <button className="btn btn-danger" onClick={handleClick}><i class="fa-solid fa-magnifying-glass"></i></button>
+           </div>
+            <div>
+              <h1 className="data-name">{data.name}</h1>
+              <h4 className="data-cel">{data.celcius}°</h4>
+              <p className="data-weth">{data.climate}</p>
+            </div>
+            <div className="row">
+
+            <div className="col text-center">
+             <p  className="data-spd">Speed</p>
+             <p className="data-spd">{data.speed} km/h <i class="fa-solid fa-wind"></i></p>
+            </div>
+
+            <div className="col text-center">
+              <p className="data-humi">Humidity</p>
+              <p className="data-hum"> {data.hum} % <i class="fa-solid fa-temperature-arrow-down"></i></p>
+             
+            </div>
+
+            </div>  
+
+             </div>
+             
+           
+
+          </div>
+
+          
+          </div>
+          </div>
+        </div>
+    </section>
   );
 }
 
